da76042d9096ccb9e08028e701c4b0717bdb0cde
Brandon <iambj@gmail.com>
2019-03-13 17:27:33 -0400

    Tidied up the code.

--- fixwords.py
+++ fixwords.py
@@ -1,17 +1,15 @@
+# Takes a long \n delimited list of words and converts it to a JS file with 1 array filled with said words.
 import os
-
 count = 0
 words = []
 with open("words.txt") as f:
     for line in f:
         line = line.strip()
+        # We're only going to used words that are > 8 in length.
         if len(line) >= 8:
             print()
             words.append("{}".format(line))
             count += 1
-
-print(count, "words.")
-print(words)
 
 file = open("words.js", "w")
 file.write("words = " + str(words))
--- gen.js
+++ gen.js
@@ -1,74 +1,74 @@
-//JS!
+// Generates "entropy" by moving the mouse around.
 var targID = "data";
 var target = document.getElementById(targID);
 var btn = document.getElementById(genBtn);
 var progBar = document.getElementById("inner");
 var entLength = document.getElementById("ent");
-var entropyLength = 1024*0.25 //1kbyte in bits
+var results = document.getElementById("results");
+var entropyLength = 1024 * 0.5; //1kbyte in bits
 var pool = "";
 
 function updateDiv(t, f) {
     t.innerHTML = f;
-        
 }
 
-genBtn.addEventListener('click', generate, false);
+genBtn.addEventListener("click", generate, false);
 function entropy(event) {
     //updateDiv(target, event.clientX + event.clientY);
-    
-    if(pool.length >= entropyLength){
-        pool = pool.substr(0,entropyLength);
+
+    if (pool.length >= entropyLength) {
+        pool = pool.substr(0, entropyLength);
         progBar.style.width = "100%";
         progBar.style.background = "green";
-        document.removeEventListener('mousemove',entropy, false);
-    }else{
-        pool += "" + event.clientX%7 + event.clientY;
-        progBar.style.width = pool.length/entropyLength*100 + "%";
-        updateDiv(entLength, (pool.length >= 1024 ? 1024 : pool.length));
-    };
+        document.removeEventListener("mousemove", entropy, false);
+    } else {
+        pool += "" + (event.clientX % 7) + event.clientY;
+        progBar.style.width = (pool.length / entropyLength) * 100 + "%";
+        updateDiv(entLength, pool.length >= 1024 ? 1024 : pool.length);
+    }
 }
 
-
-
-document.addEventListener('mousemove', function(e){
-    throttle(20, false, entropy, e);
-}, false);
-
+document.addEventListener(
+    "mousemove",
+    function(e) {
+        throttle(20, false, entropy, e);
+    },
+    false
+);
 
 var throttled = []; //global holding cell for scope.
-var throttle = function(time, scope, callback, args){
-    if(throttled.length >= 1){
-        for(var i = 0; i <= throttle.length; i++){
-            if(throttled[i] === scope){
-               // console.log("you've been throttled!");
+var throttle = function(time, scope, callback, args) {
+    if (throttled.length >= 1) {
+        for (var i = 0; i <= throttle.length; i++) {
+            if (throttled[i] === scope) {
+                // console.log("you've been throttled!");
                 return false;
             }
         }
     }
     throttled.push(scope);
-    var timer = setTimeout(function () {
+    var timer = setTimeout(function() {
         throttled.splice(throttled.indexOf(scope, 1));
-        if(typeof callback === "function"){
+        if (typeof callback === "function") {
             callback(args);
         }
         return null;
-    }, time)
+    }, time);
     return true;
 };
 
-function pickWord(){
-    num = Math.floor(Math.random() * words.length)
+function pickWord() {
+    num = Math.floor(Math.random() * words.length);
     return words[num];
 }
 
-function generate(){
-    if(pool.length >= entropyLength){
-        var one = (pool.charAt(Math.floor(Math.random()*100)));
-        var two = (pool.charAt(Math.floor(Math.random()*100)));
-        var three = (pool.charAt(Math.floor(Math.random()*100)));
-        var four = (pool.charAt(Math.floor(Math.random()*100)));
-        console.log("" + one + two + pickWord() + three + four);
+function generate() {
+    if (pool.length >= entropyLength) {
+        var one = pool.charAt(Math.floor(Math.random() * pool.length));
+        var two = pool.charAt(Math.floor(Math.random() * pool.length));
+        var three = pool.charAt(Math.floor(Math.random() * pool.length));
+        var four = pool.charAt(Math.floor(Math.random() * pool.length));
+        results.innerHTML += "" + one + two + pickWord() + three + four + "</br>";
     }
-
 }
-console.log(words)
+
